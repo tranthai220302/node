95f8503d58fd0214141f14cf02baedfbecb352a5
Tran Quang THAI <tranthai220203@gmail.com>
2022-10-29 14:16:00 +0700

    video 6

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+import configViewEngine from './configs/viewengine';
+
+const app = express();
+const port = 8080
+
+configViewEngine(app);
+app.get('/',(req, res) =>
+{
+    res.render('index.ejs')
+})
+
+app.listen(port, () =>{
+    console.log(`Example app listening at http://localhost:${port}`);
+})
